f23da6832316f4aade0694fb07ed7b16af0d23f7
Karczel <92095164+Karczel@users.noreply.github.com>
2024-11-19 10:08:39 +0700

    Amazon S3 integration
    #33
    + do upload/delete file with s3 client in yumelinkapp.utils.utils
    + logging

--- yumelinkapp/utils/utils.py
+++ yumelinkapp/utils/utils.py
@@ -1,6 +1,26 @@
+import boto3
+
+from yumelink import settings
+
 SMALL_TEXT = 100
 MID_SMALL_TEXT = 500
 MID_BIG_TEXT = 2000
 BIG_TEXT = 5000
 
 UNTITLED = 'Untitled'
+
+
+def get_s3_client():
+    """Get S3 client for authentication to access S3 storage."""
+    return boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME
+    )
+
+
+s3_client = get_s3_client()
+BUCKET_NAME = settings.AWS_STORAGE_BUCKET_NAME
+
+# use s3_client to upload/delete image
