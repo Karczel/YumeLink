c3297f0958efd722f7cd2b5bce5340571706874d
Kantapon Hemmadhun <kantapon.hemmadhun@gmail.com>
2024-11-26 12:40:59 +0700

    Merge final code

--- yumelinkapp/views/profile_view.py
+++ yumelinkapp/views/profile_view.py
@@ -12,17 +12,23 @@
 #         return context
 
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
+from yumelinkapp.models import Follow, Block, User
+
 
 class ProfileView(TemplateView):
     template_name = 'user/user_profile.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        target_user = kwargs.get('target_user', None)
+        context['target_user'] = target_user
+
         if self.request.user.is_authenticated:
-            # Pass the logged-in user's information if they are authenticated
             context['user_profile'] = self.request.user
         else:
-            # Provide default values for unauthenticated users
             context['user_profile'] = {
                 'name': 'Guest User',
                 'email': 'N/A',
