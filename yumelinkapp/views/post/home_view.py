077af2ebf50b3151454109610a0692ee8302ecec
Karczel <92095164+Karczel@users.noreply.github.com>
2024-11-29 08:02:24 +0700

    Search by tags and filter content

--- yumelinkapp/views/post/home_view.py
+++ yumelinkapp/views/post/home_view.py
@@ -1,40 +1,53 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from yumelinkapp.models import Post, PostImage, User, Block
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Post
     template_name = 'yumelink/home.html'
+    context_object_name = 'posts_with_images'
 
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        return render(request, self.template_name, context)
+    def get_queryset(self):
+        search_query = self.request.GET.get('search', '')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
         try:
             current_user = User.objects.get(id=self.request.user.id)
             blocking_users = Block.objects.filter(blocked=current_user).values_list('blocker', flat=True)
             blocked_users = Block.objects.filter(blocker=current_user).values_list('blocked', flat=True)
             excluded_users = set(blocking_users) | set(blocked_users)
 
-            posts = Post.objects.all().exclude(user__id__in=excluded_users).order_by('-timestamp')
+            if search_query:
+                # Filter posts by content if a search query is provided
+                queryset = Post.objects.filter(
+                    Q(content__icontains=search_query) |  # Filter by content
+                    Q(posttag__tag__content__icontains=search_query) |  # Filter by tag name (assuming you have a Many-to-Many relation 'tags')
+                    Q(user__username__icontains=search_query) |  # Filter by username
+                    Q(user__name__icontains=search_query)
+                ).exclude(
+                    user__id__in=excluded_users).order_by('-timestamp')
+            else:
+                queryset = Post.objects.all().exclude(user__id__in=excluded_users).order_by('-timestamp')
 
         except User.DoesNotExist:
-            posts = Post.objects.all().order_by('-timestamp')
+            queryset = Post.objects.all().order_by('-timestamp')
 
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_query = self.request.GET.get('search', '')
+        context['search_query'] = search_query
 
         context['posts_with_images'] = [
             {
                 'post': post,
                 'post_images': PostImage.objects.filter(post=post)
             }
-            for post in posts
+            for post in self.get_queryset()
         ]
         return context
 
-    def post(self, request, *args, **kwargs):
-        # add create post as request post/use url create_post
-        return HttpResponse(self.get(request))
